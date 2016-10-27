10626c984d3560565bf6e18e93230917e12b0cee
juleasGoullee <julesgoullee@gmail.com>
2016-10-27 16:44:18 +0200

    config env http

--- front/src/config.js
+++ front/src/config.js
@@ -1,5 +1,5 @@
 export default {
   'ENV': process.env.NODE_ENV || 'development',
-  'API_HOST': 'v3.soundcharts.com:6789',
+  'API_HOST': '//v3.soundcharts.com:6789',
   'LINK_HOST': document.location.host,
 };
