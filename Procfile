13c6c386024947ad4e2421aef20ed0cffcd7fe7d
juleasGoullee <julesgoullee@gmail.com>
2016-10-29 07:28:14 +0200

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node api/index.js
+web: API_PORT=${PORT} node api/index.js
--- front/src/config.js
+++ front/src/config.js
@@ -1,5 +1,5 @@
 export default {
   'ENV': process.env.NODE_ENV || 'development', //eslint-disable-line no-process-env
-  'API_HOST': '//v3.soundcharts.com:6789',
+  'API_HOST': '//hell-wheel.herokuapp.com',
   'LINK_HOST': document.location.host
 };
